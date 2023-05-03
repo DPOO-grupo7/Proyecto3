e89778d858651666c29a5fec5b6c46912a032da8
Leonardo3310 <jl.rangel@uniandes.edu.co>
2023-05-03 18:03:35 -0500

    Log In + Registro sin terminar

--- src/vista/LogIn.java
+++ src/vista/LogIn.java
@@ -15,42 +15,52 @@ public class LogIn extends JFrame implements ActionListener {
     private JPasswordField passwordField;
     private JButton loginButton;
     private JPanel loginPanel;
+    private JButton registerButton;
     private Autenticador Autenticador = new Autenticador();
 
     public LogIn() {
         // Configurar el JFrame
-        setTitle("inicio de sesión");
+        setTitle("Inicio de sesión");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 500);
         setLocationRelativeTo(null); // Centrar la ventana en la pantalla
 
         // Crear el panel de inicio de sesión
         loginPanel = new JPanel();
-        loginPanel.setLayout(new GridLayout(3, 2, 10, 10));
+        loginPanel.setLayout(new GridLayout(3, 2));
+        
 
         // Crear los componentes del panel
-        JLabel usernameLabel = new JLabel("Nombre de usuario:");
-        JLabel passwordLabel = new JLabel("Contraseña:");
+        JLabel usernameLabel = new JLabel("Usuario:");
+        loginPanel.add(usernameLabel);
+
         usernameField = new JTextField();
+        loginPanel.add(usernameField);
+
+        JLabel passwordLabel = new JLabel("Contraseña:");
+        loginPanel.add(passwordLabel);
+
         passwordField = new JPasswordField();
+        loginPanel.add(passwordField);
+
         loginButton = new JButton("Iniciar sesión");
         loginButton.setBackground(Color.decode("#486c84"));
-        //loginButton.setBorderPainted(false);
         loginButton.addActionListener(this);
-        
-        loginPanel.setBackground(Color.decode("#a8c4d4"));
-        loginPanel.setMaximumSize(new Dimension(300, 100));
-        // Agregar los componentes al panel
-        loginPanel.add(usernameLabel);
-        loginPanel.add(usernameField);
-        loginPanel.add(passwordLabel);
-        loginPanel.add(passwordField);
-        loginPanel.add(new JLabel(""));
         loginPanel.add(loginButton);
 
+        registerButton = new JButton("Registrarse");
+        registerButton.setBackground(Color.decode("#486c84"));
+        registerButton.addActionListener(this);
+        loginPanel.add(registerButton);
+        
+        
+
+        loginPanel.setBackground(Color.decode("#a8c4d4"));
+        loginPanel.setMaximumSize(new Dimension(300, 100));
         // Agregar el panel al JFrame
         add(loginPanel, BorderLayout.CENTER);
 
+        
         // Mostrar el JFrame
         setVisible(true);
      
@@ -62,15 +72,20 @@ public class LogIn extends JFrame implements ActionListener {
     	String username = usernameField.getText();
         String password = new String(passwordField.getPassword());
         
-        if (this.Autenticador.autenticar(username, password) == false)
-        {
-        	JOptionPane.showMessageDialog(this, "Invalid username or password.", "Error", JOptionPane.ERROR_MESSAGE);
+        if (e.getSource() == loginButton)
+        
+	        if (this.Autenticador.autenticar(username, password) == false)
+	        {
+	        	JOptionPane.showMessageDialog(this, "Invalid username or password.", "Error", JOptionPane.ERROR_MESSAGE);
+	        }
+	        else
+	        {
+	        	JOptionPane.showMessageDialog(this, "Login successful!");
         }
-        else
+        else if (e.getSource() == registerButton)
         {
-        	JOptionPane.showMessageDialog(this, "Login successful!");
+        	
         }
-    	
     }
 
 	
