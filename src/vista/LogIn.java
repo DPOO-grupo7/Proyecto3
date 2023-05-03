0127b98da041daf0fd4bd71952941d9795d5c7a2
Leonardo3310 <jl.rangel@uniandes.edu.co>
2023-05-03 15:44:14 -0500

    LogIn

--- src/vista/LogIn.java
+++ src/vista/LogIn.java
@@ -1,10 +1,52 @@
 package vista;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
 
-public class LogIn extends JPanel{
+public class LogIn extends JFrame{
+	
+	JFrame frame;
+	JTextField usuario;
+	JPasswordField contraseña;
+	JLabel label;
+	ImageIcon hoteldpo;
+	JButton iniciar;
+	
 
 	public LogIn() {
+		frame = new JFrame();
+		frame.setVisible(true);
+		frame.setTitle("Hotel ddpo");
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setSize(800, 700);
+		frame.setResizable(true);
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.setBackground(Color.decode("#a8c4d4"));
 		
+		this.usuario = new JTextField();
+		usuario.setPreferredSize(new Dimension (60, 100));
+		this.contraseña = new JPasswordField();
+		this.label = new JLabel("hi");
+		this.hoteldpo = new ImageIcon("./imagenes/Foto Dpoo.png");
+		label.setIcon(this.hoteldpo);
+		this.iniciar = new JButton();
+		
+
+		panel.add(Box.createVerticalGlue());
+		panel.add(label);
+		panel.add(Box.createVerticalGlue());
+		panel.add(usuario);
+		panel.add(Box.createVerticalGlue());
+		panel.add(contraseña);
+		panel.add(Box.createVerticalGlue());
+		panel.add(iniciar);
+		panel.add(Box.createVerticalGlue());
+		panel.setAlignmentY(CENTER_ALIGNMENT);
+		panel.setAlignmentX(CENTER_ALIGNMENT);
+		panel.setVisible(true);
+		frame.add(panel);
 	}
 }
