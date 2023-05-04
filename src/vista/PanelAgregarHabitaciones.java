177d7c25569f9a4bde6bdd93cfaa9d0e4953d7e9
Leonardo3310 <jl.rangel@uniandes.edu.co>
2023-05-04 14:38:22 -0500

    si lo dejo para luego pielde

---
+++ src/vista/PanelAgregarHabitaciones.java
@@ -0,0 +1,26 @@
+package vista;
+
+import java.awt.Color;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import java.awt.*;
+
+import utilidades.Autenticador;
+
+public class PanelAgregarHabitaciones extends JFrame {
+
+	private JComboBox<String> Desplegable;
+	private JButton TipoHabitacion;
+	private JButton NumeroHabitacion;
+	private JButton Tarifa;
+	
+	
+	
+	public PanelAgregarHabitaciones()
+	{
+		
+	}
+}
--- src/vista/main.java
+++ src/vista/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new LogIn();
+		//este main es para pobrobar cada ventana
 	}
 
 }
